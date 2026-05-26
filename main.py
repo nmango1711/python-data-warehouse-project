a07549391c549a4b0fb93ae1e7f43f88f356ad41
nmango1711 <janicijevic.stefi@gmail.com>
2026-05-26 13:11:14 +0200

    add loc_a101 silver

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from src.transform.silver.transform_silver_cust_info import transform_cust_info_
 from src.transform.silver.transform_silver_prd_info import transform_prd_info_to_silver
 from src.transform.silver.transform_silver_sales_details import transform_sales_details_to_silver
 from src.transform.silver.transform_silver_cust_az12 import transform_cust_az12_to_silver
+from src.transform.silver.transform_silver_loc_a101 import transform_loc_a101_to_silver
+
+
 SOURCE_PATH = "data/source"
 BRONZE_PATH = "data/bronze"
 SILVER_PATH = "data/silver"
@@ -11,7 +14,8 @@ SILVER_PATH = "data/silver"
 # transform_cust_info_to_silver(BRONZE_PATH, SILVER_PATH)
 # transform_prd_info_to_silver(BRONZE_PATH, SILVER_PATH)
 # transform_sales_details_to_silver(BRONZE_PATH, SILVER_PATH)
-transform_cust_az12_to_silver(BRONZE_PATH, SILVER_PATH)
+# transform_cust_az12_to_silver(BRONZE_PATH, SILVER_PATH)
+transform_loc_a101_to_silver(BRONZE_PATH, SILVER_PATH)
 
 # from src.config.spark_session import get_spark
 # spark = get_spark()
