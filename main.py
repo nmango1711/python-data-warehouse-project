f653c862adf9c8fe5df01d8ac9eb7d635579fc05
nmango1711 <janicijevic.stefi@gmail.com>
2026-05-26 13:29:34 +0200

    add px_cat_g1v2 silver

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from src.ingest.load_to_bronze import ingest_csv_to_bronze
-from src.transform.silver.transform_silver_cust_info import transform_cust_info_to_silver
-from src.transform.silver.transform_silver_prd_info import transform_prd_info_to_silver
-from src.transform.silver.transform_silver_sales_details import transform_sales_details_to_silver
-from src.transform.silver.transform_silver_cust_az12 import transform_cust_az12_to_silver
-from src.transform.silver.transform_silver_loc_a101 import transform_loc_a101_to_silver
-
+from src.transform.silver.run_silver import run_silver
 
 SOURCE_PATH = "data/source"
 BRONZE_PATH = "data/bronze"
-SILVER_PATH = "data/silver"
-# ingest_csv_to_bronze(SOURCE_PATH, BRONZE_PATH)
 
-# transform_cust_info_to_silver(BRONZE_PATH, SILVER_PATH)
-# transform_prd_info_to_silver(BRONZE_PATH, SILVER_PATH)
-# transform_sales_details_to_silver(BRONZE_PATH, SILVER_PATH)
-# transform_cust_az12_to_silver(BRONZE_PATH, SILVER_PATH)
-transform_loc_a101_to_silver(BRONZE_PATH, SILVER_PATH)
+#ingest_csv_to_bronze(SOURCE_PATH, BRONZE_PATH)
+run_silver()
 
 # from src.config.spark_session import get_spark
 # spark = get_spark()
